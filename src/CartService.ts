9db604bb6c869c204764eb1cc253141a93faa558
Sigma wadbude <sigmawadbude@gmail.com>
2025-07-20 17:49:21 -0400

    feat: Enhance addToCart method to include API call for cart updates

--- src/CartService.ts
+++ src/CartService.ts
@@ -1,10 +1,17 @@
 import type { IProduct } from "./product.model";
-
+import axios from "axios";
 class CartService {
   private cart: IProduct[] = [];
 
   addToCart(product: IProduct): void {
     this.cart.push(product);
+    axios.post('/api/cart', product)
+      .then(response => {
+        console.log("Product added to cart:", response.data);
+      })
+      .catch(error => {
+        console.error("Error adding product to cart:", error);
+      });
     console.log("Cart:", this.cart);
   }
 
